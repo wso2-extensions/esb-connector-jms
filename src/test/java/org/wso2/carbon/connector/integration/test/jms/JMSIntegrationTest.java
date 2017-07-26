d2abeec1cdc6a0197ac618ee7736d9652b6e49e5
Rajjaz <hmrajas1990@gmail.com>
2017-07-26 18:20:44 +0530

    Integration test version changed to 1.0.1-SNAPSHOT

--- src/test/java/org/wso2/carbon/connector/integration/test/jms/JMSIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/jms/JMSIntegrationTest.java
@@ -37,7 +37,7 @@ public class JMSIntegrationTest extends ConnectorIntegrationTestBase {
 
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
-        init("jms-connector-1.0.0-SNAPSHOT");
+        init("jms-connector-1.0.1-SNAPSHOT");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
         esbRequestHeadersMap.put("Accept", "application/json");
