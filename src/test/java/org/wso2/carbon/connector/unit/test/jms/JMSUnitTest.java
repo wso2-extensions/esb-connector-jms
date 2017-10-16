8a23801f7faf56b706da7aac2add818a2a10fcf6
sivaramya <sivaramya14@gmail.com>
2017-10-16 10:29:47 +0530

    Adding test cases

--- src/test/java/org/wso2/carbon/connector/unit/test/jms/JMSUnitTest.java
+++ src/test/java/org/wso2/carbon/connector/unit/test/jms/JMSUnitTest.java
@@ -24,8 +24,10 @@ import org.apache.activemq.broker.TransportConnector;
 import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMDocument;
 import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.OMFactory;
 import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.SynapseException;
 import org.apache.synapse.config.SynapseConfiguration;
@@ -42,19 +44,15 @@ import org.wso2.carbon.connector.jms.JMSConnectorConstants;
 import org.wso2.carbon.connector.jms.JMSInitializer;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 
-import java.io.StringReader;
 import javax.jms.Connection;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
 
 public class JMSUnitTest {
     private JMSConnector jmsConnector;
     private JMSInitializer jmsInitializer;
     private MessageContext messageContext;
-    protected BrokerService broker;
+    private BrokerService broker;
     private Connection connection;
-    protected TransportConnector connector;
+    private TransportConnector connector;
 
     @BeforeClass
     public void setup() throws Exception {
@@ -101,6 +99,7 @@ public class JMSUnitTest {
         messageContext.setProperty(JMSConnectorConstants.JAVA_NAMING_PROVIDER_URL, "vm://localhost");
         messageContext.setProperty(JMSConnectorConstants.CONNECTION_POOL_SIZE, "10000");
         messageContext.setProperty(JMSConnectorConstants.JMS_MESSAGE_TYPE, "ByteMessage");
+        messageContext.setProperty(JMSConnectorConstants.JMS_PRIORITY, "1");
         jmsInitializer.connect(messageContext);
         jmsConnector.connect(messageContext);
         Assert.assertEquals((messageContext.getEnvelope().getBody().getFirstElement()).getText() != null,
@@ -109,7 +108,7 @@ public class JMSUnitTest {
 
     @Test
     public void publishMessage2() throws AxisFault, ConnectException {
-        messageContext = createMessageContext();
+        messageContext = createNewMessageContext2();
         messageContext.setProperty(JMSConnectorConstants.DESTINATION_NAME, "JMSTest");
         messageContext.setProperty(JMSConnectorConstants.DESTINATION_TYPE, "topic");
         messageContext.setProperty(JMSConnectorConstants.CONNECTION_FACTORY_NAME, "TopicConnectionFactory");
@@ -127,6 +126,82 @@ public class JMSUnitTest {
         messageContext.setProperty(JMSConnectorConstants.JMS_TIMESTAMP, "1072017");
         messageContext.setProperty(JMSConnectorConstants.JMS_DELIVERY_MODE, "1");
         messageContext.setProperty(JMSConnectorConstants.JMS_CORRELATION_ID, "10");
+        messageContext.setProperty(JMSConnectorConstants.JMS_MESSAGE_TYPE, "binary");
+        messageContext.setProperty(JMSConnectorConstants.JMS_EXPIRATION, "1000");
+        jmsInitializer.connect(messageContext);
+        jmsConnector.connect(messageContext);
+        Assert.assertEquals((messageContext.getEnvelope().getBody().getFirstElement()).getText() != null,
+                            true);
+    }
+
+    @Test
+    public void publishMessage3() throws AxisFault, ConnectException {
+        messageContext = createNewMessageContext();
+        messageContext.setProperty(JMSConnectorConstants.DESTINATION_NAME, "JMSTest");
+        messageContext.setProperty(JMSConnectorConstants.DESTINATION_TYPE, "queue");
+        messageContext.setProperty(JMSConnectorConstants.CONNECTION_FACTORY_NAME, "QueueConnectionFactory");
+        messageContext.setProperty
+                (JMSConnectorConstants.JAVA_NAMING_FACTORY_INITIAL,
+                 "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        messageContext.setProperty(JMSConnectorConstants.JAVA_NAMING_PROVIDER_URL, "vm://localhost");
+        messageContext.setProperty(JMSConnectorConstants.CONNECTION_POOL_SIZE, "10000");
+        messageContext.setProperty(JMSConnectorConstants.DELIVERY_MODE, "1");
+        messageContext.setProperty(JMSConnectorConstants.TIME_TO_LIVE, "500");
+        messageContext.setProperty(JMSConnectorConstants.PRIORITY, "1");
+        messageContext.setProperty(JMSConnectorConstants.USERNAME, "guest");
+        messageContext.setProperty(JMSConnectorConstants.PASSWORD, "guest");
+        messageContext.setProperty(JMSConnectorConstants.JMS_PRIORITY, "1");
+        messageContext.setProperty(JMSConnectorConstants.JMS_TIMESTAMP, "1072017");
+        messageContext.setProperty(JMSConnectorConstants.JMS_DELIVERY_MODE, "1");
+        messageContext.setProperty(JMSConnectorConstants.JMS_CORRELATION_ID, "10");
+        messageContext.setProperty(JMSConnectorConstants.JMS_EXPIRATION, "1000");
+        jmsInitializer.connect(messageContext);
+        jmsConnector.connect(messageContext);
+        Assert.assertEquals((messageContext.getEnvelope().getBody().getFirstElement()).getText() != null,
+                            true);
+    }
+
+    @Test
+    public void publishMessage4() throws AxisFault, ConnectException {
+        messageContext = createNewMessageContext1();
+        messageContext.setProperty(JMSConnectorConstants.DESTINATION_NAME, "JMSTest");
+        messageContext.setProperty(JMSConnectorConstants.DESTINATION_TYPE, "queue");
+        messageContext.setProperty(JMSConnectorConstants.CONNECTION_FACTORY_NAME, "QueueConnectionFactory");
+        messageContext.setProperty
+                (JMSConnectorConstants.JAVA_NAMING_FACTORY_INITIAL,
+                 "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        messageContext.setProperty(JMSConnectorConstants.JAVA_NAMING_PROVIDER_URL, "vm://localhost");
+        messageContext.setProperty(JMSConnectorConstants.CONNECTION_POOL_SIZE, "10000");
+        messageContext.setProperty(JMSConnectorConstants.DELIVERY_MODE, "1");
+        messageContext.setProperty(JMSConnectorConstants.TIME_TO_LIVE, "500");
+        messageContext.setProperty(JMSConnectorConstants.PRIORITY, "1");
+        messageContext.setProperty(JMSConnectorConstants.USERNAME, "guest");
+        messageContext.setProperty(JMSConnectorConstants.PASSWORD, "guest");
+        messageContext.setProperty(JMSConnectorConstants.JMS_PRIORITY, "1");
+        messageContext.setProperty(JMSConnectorConstants.JMS_TIMESTAMP, "1072017");
+        messageContext.setProperty(JMSConnectorConstants.JMS_DELIVERY_MODE, "1");
+        messageContext.setProperty(JMSConnectorConstants.JMS_CORRELATION_ID, "10");
+        messageContext.setProperty(JMSConnectorConstants.JMS_MESSAGE_TYPE, "binary");
+        messageContext.setProperty(JMSConnectorConstants.JMS_EXPIRATION, "1000");
+        jmsInitializer.connect(messageContext);
+        jmsConnector.connect(messageContext);
+        Assert.assertEquals((messageContext.getEnvelope().getBody().getFirstElement()).getText() != null,
+                            true);
+    }
+
+    @Test
+    public void publishMessage5() throws AxisFault, ConnectException {
+        messageContext = createMessageContext();
+        messageContext.setProperty(JMSConnectorConstants.DESTINATION_NAME, "JMSTest");
+        messageContext.setProperty(JMSConnectorConstants.DESTINATION_TYPE, "topic");
+        messageContext.setProperty(JMSConnectorConstants.CONNECTION_FACTORY_NAME, "TopicConnectionFactory");
+        messageContext.setProperty
+                (JMSConnectorConstants.JAVA_NAMING_FACTORY_INITIAL,
+                 "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+        messageContext.setProperty(JMSConnectorConstants.JAVA_NAMING_PROVIDER_URL, "vm://localhost");
+        messageContext.setProperty(JMSConnectorConstants.CONNECTION_POOL_SIZE, "10000");
+        ((Axis2MessageContext)messageContext).getAxis2MessageContext().setProperty
+                (JMSConnectorConstants.JMS_MESSAGE_TYPE, "ByteMessage");
         jmsInitializer.connect(messageContext);
         jmsConnector.connect(messageContext);
         Assert.assertEquals((messageContext.getEnvelope().getBody().getFirstElement()).getText() != null,
@@ -136,28 +211,67 @@ public class JMSUnitTest {
     protected MessageContext createMessageContext() throws AxisFault {
         org.apache.axis2.context.MessageContext mc = new org.apache.axis2.context.MessageContext();
         SynapseConfiguration config = new SynapseConfiguration();
+        AxisConfiguration axisConfiguration = new AxisConfiguration();
+        ConfigurationContext context = new ConfigurationContext(axisConfiguration);
+        mc.setConfigurationContext(context);
         SynapseEnvironment env = new Axis2SynapseEnvironment(config);
         MessageContext messageContext = new Axis2MessageContext(mc, config, env);
         org.apache.axiom.soap.SOAPEnvelope envelope = OMAbstractFactory.getSOAP11Factory().getDefaultEnvelope();
         OMDocument omDoc = OMAbstractFactory.getSOAP11Factory().createOMDocument();
+        OMFactory omFactory = envelope.getOMFactory();
         omDoc.addChild(envelope);
-        envelope.getBody().addChild(createOMElement("<a>test</a>"));
+        OMElement newElement = omFactory.createOMElement("binary", null);
+        envelope.getBody().addChild(newElement);
         messageContext.setEnvelope(envelope);
         return messageContext;
     }
 
-    public static OMElement createOMElement(String xml) {
-        try {
-            XMLStreamReader xmlStreamReader =
-                    XMLInputFactory.newInstance().createXMLStreamReader(new StringReader(xml));
-            StAXOMBuilder builder = new StAXOMBuilder(xmlStreamReader);
-            return builder.getDocumentElement();
-        } catch (XMLStreamException e) {
-            throw new RuntimeException(e);
-        }
+    protected MessageContext createNewMessageContext() throws AxisFault {
+        org.apache.axis2.context.MessageContext mc = new org.apache.axis2.context.MessageContext();
+        SynapseConfiguration config = new SynapseConfiguration();
+        SynapseEnvironment env = new Axis2SynapseEnvironment(config);
+        MessageContext messageContext = new Axis2MessageContext(mc, config, env);
+        org.apache.axiom.soap.SOAPEnvelope envelope = OMAbstractFactory.getSOAP11Factory().getDefaultEnvelope();
+        OMDocument omDoc = OMAbstractFactory.getSOAP11Factory().createOMDocument();
+        OMFactory omFactory = envelope.getOMFactory();
+        omDoc.addChild(envelope);
+        OMElement newElement = omFactory.createOMElement(JMSConnectorConstants.DEFAULT_BINARY_WRAPPER);
+        envelope.getBody().addChild(newElement);
+        messageContext.setEnvelope(envelope);
+        return messageContext;
     }
 
-    protected BrokerService createBroker() throws Exception {
+    protected MessageContext createNewMessageContext1() throws AxisFault {
+        org.apache.axis2.context.MessageContext mc = new org.apache.axis2.context.MessageContext();
+        SynapseConfiguration config = new SynapseConfiguration();
+        SynapseEnvironment env = new Axis2SynapseEnvironment(config);
+        MessageContext messageContext = new Axis2MessageContext(mc, config, env);
+        org.apache.axiom.soap.SOAPEnvelope envelope = OMAbstractFactory.getSOAP11Factory().getDefaultEnvelope();
+        OMDocument omDoc = OMAbstractFactory.getSOAP11Factory().createOMDocument();
+        OMFactory omFactory = envelope.getOMFactory();
+        omDoc.addChild(envelope);
+        OMElement newElement = omFactory.createOMElement(JMSConnectorConstants.DEFAULT_TEXT_WRAPPER);
+        envelope.getBody().addChild(newElement);
+        messageContext.setEnvelope(envelope);
+        return messageContext;
+    }
+
+    protected MessageContext createNewMessageContext2() throws AxisFault {
+        org.apache.axis2.context.MessageContext mc = new org.apache.axis2.context.MessageContext();
+        SynapseConfiguration config = new SynapseConfiguration();
+        SynapseEnvironment env = new Axis2SynapseEnvironment(config);
+        MessageContext messageContext = new Axis2MessageContext(mc, config, env);
+        org.apache.axiom.soap.SOAPEnvelope envelope = OMAbstractFactory.getSOAP11Factory().getDefaultEnvelope();
+        OMDocument omDoc = OMAbstractFactory.getSOAP11Factory().createOMDocument();
+        OMFactory omFactory = envelope.getOMFactory();
+        omDoc.addChild(envelope);
+        OMElement newElement = omFactory.createOMElement(JMSConnectorConstants.MAP_QNAME);
+        envelope.getBody().addChild(newElement);
+        messageContext.setEnvelope(envelope);
+        return messageContext;
+    }
+
+    private BrokerService createBroker() throws Exception {
         BrokerService service = new BrokerService();
         service.setPersistent(false);
         service.setUseJmx(false);
